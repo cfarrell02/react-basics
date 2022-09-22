f998a3baef806f97689f489171f370d02dd45f05
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-09-22 11:36:58 +0100

    Exercise 2 completed.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const Modules = [{name: "Devops",noLectures: 2, noPracticals: 2},
+  {name: "Enterprise Web Dev",noLectures: 3, noPracticals: 2}]
   return (
+    <div>
       <h1>TODO</h1>
+      <table className="table table-bordered">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>No lectures</th>
+      <th>No practicals</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <td>{Modules[0].name}</td>
+      <td>{Modules[0].noLectures}</td>
+      <td>{Modules[0].noPracticals}</td>
+    </tr>
+    <tr>
+      <td>{Modules[1].name}</td>
+      <td>{Modules[1].noLectures}</td>
+      <td>{Modules[1].noPracticals}</td>
+    </tr>
+  </tbody >
+</table>
+</div>
+
   );
 };
 
